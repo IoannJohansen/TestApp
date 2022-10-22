b518278c27eb50c37930ca435b99cb0b1ad0e2d3
Tomogavk <boss.vanya20@mail.ru>
2022-10-23 02:15:25 +0300

    - Minor refactoring.

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -11,10 +11,10 @@ builder.Services.AddHttpLogging(logging =>
 {
     logging.LoggingFields = HttpLoggingFields.All;
     logging.RequestHeaders.Add("sec-ch-ua");
+    logging.ResponseBodyLogLimit = 4096;
     logging.ResponseHeaders.Add("MyResponseHeader");
     logging.MediaTypeOptions.AddText("application/javascript");
     logging.RequestBodyLogLimit = 4096;
-    logging.ResponseBodyLogLimit = 4096;
 });
 
 var app = builder.Build();
